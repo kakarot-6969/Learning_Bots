df260d0326888db967c98f0f20fb4e6deb06479a
kakarot-6969 <rajsrijan51@gmail.com>
2024-07-26 09:10:16 -0700

    Update config.py kaka

--- config.py
+++ config.py
@@ -6,13 +6,13 @@ from pyrogram import filters
 
 load_dotenv()
 
-API_ID = 
-API_HASH = ""
-BOT_TOKEN = ""
-MONGO_DB_URI = ""
+API_ID =  21899392
+API_HASH = " 1784e0b77aa020165687f59f573ac783"
+BOT_TOKEN = "7237043913:AAH531b1m0ywz33zX-OgBE2dGl_7n6FenHk"
+MONGO_DB_URI = "mongodb+srv://kakarot6060:kakarot6060@cluster0.lgzyjfm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 60))
-LOG_GROUP_ID = 
-OWNER_ID = 
+LOG_GROUP_ID = -1002229322982
+OWNER_ID = 6269731241
 
 ## Fill these variables if you're deploying on heroku.
 # Your heroku app name
@@ -29,8 +29,8 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "")
-SUPPORT_GROUP = getenv("SUPPORT_GROUP", "")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/channel_work_bots")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/channel_work_bots")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
@@ -52,7 +52,7 @@ TG_VIDEO_FILESIZE_LIMIT = int(getenv("TG_VIDEO_FILESIZE_LIMIT", 2145386496))
 
 
 # Get your pyrogram v2 session from Replit
-STRING1 = ""
+STRING1 = "BQFOKIAACEchzcdelhq8aPZNijN5w6BuSWOusgn7TrObe6jdtMpuvHQGd-o6FB7Vgvc9HnIsorJH8Aot-66shKyQiAcznhCLUcJkv5XLoqce8XWOE2KKRlLVeLsCquchj8aAgfFAPR2epsid6ISdiatYpdx6Z-JCTy6LiQaO6yDAlLoj6JhASty3XmTdRmAcacaDu27ukVI1XiVVyEVAHJA62wkOhlkqmFujB39T9vbn0AiDbITjPHCMyou_2aPjEoPT37Dr8dtGpm6T7kRwVhyAmn58Ll7vcUVDM1lHxDfD9ixes8Jg9vaI0ENeg4iuqleBzXq47g22IAIUV_6eio-BK78t6gAAAAG62_5DAA"
 STRING2 = getenv("STRING_SESSION2", None)
 STRING3 = getenv("STRING_SESSION3", None)
 STRING4 = getenv("STRING_SESSION4", None)
@@ -68,21 +68,21 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://graph.org/file/d0e6aa5fc70d022a4f312.jpg"
+    "START_IMG_URL", "https://telegra.ph/file/396c004bbe73f4b808617.jpg"
 )
 PING_IMG_URL = getenv(
-    "PING_IMG_URL", "https://graph.org/file/d0e6aa5fc70d022a4f312.jpg"
+    "PING_IMG_URL", "https://telegra.ph/file/75314f38cc16cb479a9b7.jpg"
 )
-PLAYLIST_IMG_URL = "https://graph.org/file/7aa263a19bd1b8148411b.jpg"
-STATS_IMG_URL = "https://graph.org/file/f586172fe40a0b5d0b0df.jpg"
-TELEGRAM_AUDIO_URL = "https://graph.org//file/2f7debf856695e0ef0607.png"
-TELEGRAM_VIDEO_URL = "https://graph.org//file/2f7debf856695e0ef0607.png"
-STREAM_IMG_URL = "https://te.legra.ph/file/bd995b032b6bd263e2cc9.jpg"
-SOUNCLOUD_IMG_URL = "https://te.legra.ph/file/bb0ff85f2dd44070ea519.jpg"
-YOUTUBE_IMG_URL = "https://graph.org//file/2f7debf856695e0ef0607.png"
-SPOTIFY_ARTIST_IMG_URL = "https://te.legra.ph/file/37d163a2f75e0d3b403d6.jpg"
-SPOTIFY_ALBUM_IMG_URL = "https://te.legra.ph/file/b35fd1dfca73b950b1b05.jpg"
-SPOTIFY_PLAYLIST_IMG_URL = "https://te.legra.ph/file/95b3ca7993bbfaf993dcb.jpg"
+PLAYLIST_IMG_URL = "https://telegra.ph/file/396c004bbe73f4b808617.jpg"
+STATS_IMG_URL = "https://telegra.ph/file/396c004bbe73f4b808617.jpg"
+TELEGRAM_AUDIO_URL = "https://telegra.ph/file/396c004bbe73f4b808617.jpg"
+TELEGRAM_VIDEO_URL = "https://telegra.ph/file/75314f38cc16cb479a9b7.jpg"
+STREAM_IMG_URL = "https://telegra.ph/file/75314f38cc16cb479a9b7.jpg"
+SOUNCLOUD_IMG_URL = "https://telegra.ph/file/75314f38cc16cb479a9b7.jpg"
+YOUTUBE_IMG_URL = "https://telegra.ph/file/75314f38cc16cb479a9b7.jpg"
+SPOTIFY_ARTIST_IMG_URL = "https://telegra.ph/file/75314f38cc16cb479a9b7.jpg"
+SPOTIFY_ALBUM_IMG_URL = "https://telegra.ph/file/75314f38cc16cb479a9b7.jpg"
+SPOTIFY_PLAYLIST_IMG_URL = "https://telegra.ph/file/75314f38cc16cb479a9b7.jpg"
 
 
 def time_to_seconds(time):
